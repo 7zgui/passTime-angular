7d7bdff080e66dac480716019825fc9811eee795
ouokki <ouokki.7zgui@gmail.com>
2022-10-24 22:07:42 +0100

    new branch for new features

--- src/app/views/pages/register/register.component.ts
+++ src/app/views/pages/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +10,16 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
-  constructor() { }
+  signupForm = new FormGroup({
+    email:new FormControl(""),
+    username:new FormControl(""),
+    password:new FormControl(""),
+  })
+
+  constructor(private authService : AuthService, private router: Router) { }
+
+  submit(){
+    console.log(this.signupForm.controls)
+  }
 
 }
